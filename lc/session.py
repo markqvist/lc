22d5bce411926e6116c3dc34cb210746af08883c
Mark Qvist <mark@unsigned.io>
2026-03-11 22:38:56 +0100

    Cleanup

--- lc/session.py
+++ lc/session.py
@@ -156,7 +156,7 @@ class Session:
             system_template = self.jinja.from_string(resolved_context["templates"][sysprompt_key])
             self.system_prompt = system_template.render(**resolved_context)
 
-            RNS.log(f"SYSTEM PROMPT RESOLVED:\n{self.system_prompt}")
+            # RNS.log(f"SYSTEM PROMPT RESOLVED:\n{self.system_prompt}")
     
     def save(self) -> None:
         if not self.config.session.get("persistence", True): return
